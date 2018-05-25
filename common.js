03fb18211da228be58ec3ab3ff5f0bab9520d29c
RACHANA SONI <rachanasoni7@gmail.com>
2018-05-26 00:59:30 +0530

    #apply eventlistner and page reload and onhash change #hide login and register button when user is logged in

--- common.js
+++ common.js
@@ -1,170 +1,208 @@
-			$(document).ready(function () {
-				$('#navlist ul li a').click(function () {
-					$('#navlist ul li a').removeClass("active");
-					$(this).addClass("active");
-				});
+	$(document).ready(function () {
+		$('#navlist ul li a').click(function () {
+			$('#navlist ul li a').removeClass("active");
+			$(this).addClass("active");
+		});
 
 
+	});
+
+	function getCookie(cname) {
+		var name = cname + "=";
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
+	}
+
+	function submitLogin() {
+		if (validatelogin()) {
+			console.log('form submitted post validation');
+			var email = $("#email").val();
+			var password = $("#password").val();
+
+			$.ajax({
+				url: 'checkLogin.php',
+				type: 'POST',
+				data: {
+					email: email,
+					password: password
+				},
+				success: function (response) {
+					$("#result").html(response);
+					$name = getCookie('name');
+					$("#username").text($name);
+					$("#preLoginBtns").hide();
+				}
 			});
+		}
+	}
 
-			function getCookie(cname) {
-				var name = cname + "=";
-				var decodedCookie = decodeURIComponent(document.cookie);
-				var ca = decodedCookie.split(';');
-				for (var i = 0; i < ca.length; i++) {
-					var c = ca[i];
-					while (c.charAt(0) == ' ') {
-						c = c.substring(1);
-					}
-					if (c.indexOf(name) == 0) {
-						return c.substring(name.length, c.length);
-					}
-				}
-				return "";
+	//********************************************************************************
+
+
+
+	window.onload = function () {
+		//#aboutus
+		var pageName = location.hash.split('#')[1];
+		if (getCookie('name') && getCookie('name') !== 'Guest') {
+			$("#preLoginBtns").hide();
+		}
+
+		associatedPage(pageName);
+	}
+
+	window.onhashchange = function () {
+		var pageName = location.hash.split('#')[1];
+		if (getCookie('name') && getCookie('name') !== 'Guest') {
+			$("#preLoginBtns").hide();
+		}
+		associatedPage(pageName);
+	}
+
+
+	function homePage() {
+		var xhttp;
+		var redirectTo;
+		if (window.XMLHttpRequest) {
+			// code for modern browsers
+			xhttp = new XMLHttpRequest();
+		} else {
+			// code for IE6, IE5
+			xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("result").innerHTML = this.responseText;
 			}
+		};
 
-			function submitLogin() {
-				if (validatelogin()) {
-					console.log('form submitted post validation');
-					var email = $("#email").val();
-					var password = $("#password").val();
+		//if user has logged in then name in cookies must be available
 
-					$.ajax({
-						url: 'checkLogin.php',
-						type: 'POST',
-						data: {
-							email: email,
-							password: password
-						},
-						success: function (response) {
-							$("#result").html(response);
-							$name = getCookie('name');
-							$("#username").text($name);
-							//document.getElementById('username').innerHTML = $name;
-						}
-					});
-				}
+		if (getCookie('name') && getCookie('name') !== 'Guest') {
+			redirectTo = 'dashboard';
+		} else {
+			redirectTo = 'home';
+		}
+		console.log(redirectTo, location.hash);
+		xhttp.open("GET", redirectTo + ".php", true);
+		xhttp.send();
+	}
+
+	if (!location.hash) {
+		homePage();
+	}
+
+
+	//****************************************************************************************************	
+
+
+	function listbook() {
+
+		var xhttp;
+		if (window.XMLHttpRequest) {
+			// code for modern browsers
+			xhttp = new XMLHttpRequest();
+		} else {
+			// code for IE6, IE5
+			xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("slider").innerHTML = this.responseText;
 			}
+		};
+		xhttp.open("GET", "listbook.php", true);
+		xhttp.send();
 
-			//********************************************************************************
+	}
 
 
-			function homePage() {
-				var xhttp;
-				if (window.XMLHttpRequest) {
-					// code for modern browsers
-					xhttp = new XMLHttpRequest();
-				} else {
-					// code for IE6, IE5
-					xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				}
-				xhttp.onreadystatechange = function () {
-					if (this.readyState == 4 && this.status == 200) {
-						document.getElementById("result").innerHTML = this.responseText;
-					}
-				};
-				xhttp.open("GET", "home.php", true);
-				xhttp.send();
+	//****************************************************************************************************	
+
+
+
+
+
+	function associatedPage(fileName) {
+		var xhttp;
+
+		if (window.XMLHttpRequest) {
+			// code for modern browsers
+			xhttp = new XMLHttpRequest();
+		} else {
+			// code for IE6, IE5
+			xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+
+				document.getElementById("result").innerHTML = this.responseText;
 			}
+		};
+		//if user has logged in then name in cookies must be available
+		if (fileName) {
+			var formattedFileName = fileName + '.php';
+			xhttp.open("GET", formattedFileName, true);
+			xhttp.send();
+		}
 
-			homePage();
 
 
-			//****************************************************************************************************	
+
+	}
+	//**************************************************************************************************
 
 
-			function listbook() {
 
-				var xhttp;
-				if (window.XMLHttpRequest) {
-					// code for modern browsers
-					xhttp = new XMLHttpRequest();
-				} else {
-					// code for IE6, IE5
-					xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				}
-				xhttp.onreadystatechange = function () {
-					if (this.readyState == 4 && this.status == 200) {
-						document.getElementById("slider").innerHTML = this.responseText;
-					}
-				};
-				xhttp.open("GET", "listbook.php", true);
-				xhttp.send();
+	function associatedChildPage(fileName) {
+		var xhttp;
+		if (window.XMLHttpRequest) {
+			// code for modern browsers
+			xhttp = new XMLHttpRequest();
+		} else {
+			// code for IE6, IE5
+			xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
 
+				document.getElementById("childPage").innerHTML = this.responseText;
 			}
+		};
+		var formattedFileName = fileName + '.php';
+		xhttp.open("GET", formattedFileName, true);
+		xhttp.send();
 
 
-			//****************************************************************************************************	
+	}
+
+	//**************************************************************************************************
 
 
 
-
-
-			function associatedPage(fileName) {
-				var xhttp;
-				if (window.XMLHttpRequest) {
-					// code for modern browsers
-					xhttp = new XMLHttpRequest();
-				} else {
-					// code for IE6, IE5
-					xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				}
-				xhttp.onreadystatechange = function () {
-					if (this.readyState == 4 && this.status == 200) {
-
-						document.getElementById("result").innerHTML = this.responseText;
-					}
-				};
-				var formattedFileName = fileName + '.php';
-				xhttp.open("GET", formattedFileName, true);
-				xhttp.send();
-
-
+	function logoutpage() {
+		var xhttp;
+		if (window.XMLHttpRequest) {
+			// code for modern browsers
+			xhttp = new XMLHttpRequest();
+		} else {
+			// code for IE6, IE5
+			xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				window.location.href = 'index.php';
 			}
-			//**************************************************************************************************
-
-
-
-			function associatedChildPage(fileName) {
-				var xhttp;
-				if (window.XMLHttpRequest) {
-					// code for modern browsers
-					xhttp = new XMLHttpRequest();
-				} else {
-					// code for IE6, IE5
-					xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				}
-				xhttp.onreadystatechange = function () {
-					if (this.readyState == 4 && this.status == 200) {
-
-						document.getElementById("childPage").innerHTML = this.responseText;
-					}
-				};
-				var formattedFileName = fileName + '.php';
-				xhttp.open("GET", formattedFileName, true);
-				xhttp.send();
-
-
-			}
-
-			//**************************************************************************************************
-
-
-
-			function logoutpage() {
-				var xhttp;
-				if (window.XMLHttpRequest) {
-					// code for modern browsers
-					xhttp = new XMLHttpRequest();
-				} else {
-					// code for IE6, IE5
-					xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				}
-				xhttp.onreadystatechange = function () {
-					if (this.readyState == 4 && this.status == 200) {
-						window.location.href = 'index.php';
-					}
-				};
-				xhttp.open("GET", "logout.php", true);
-				xhttp.send();
-			}
+		};
+		xhttp.open("GET", "logout.php", true);
+		xhttp.send();
+	}
--- header.php
+++ header.php
@@ -2,52 +2,61 @@
 session_start();
 
 ?>
-		<header>
-			<div class="col-sm-8" id="searchbar">
-				<form method="POST" action="searchbook.php">
-					<h5 style="font-weight: bold; font-size:18px;">Find Any Book of Any Author &nbsp; <span><input type="text"  id="searchbox" name="author" required></span>
-						<button id="searchbtn" type="submit">Search</button></h5>
-				</form>
-			</div>
-			<div class="col-sm-4">
-			
-			
-			<h4 id="welcometext">Welcome &nbsp;<span id="username">Guest</span>&nbsp;<i class="fa fa-user-circle" style="font-size:22px;"></i></h4>
-			</div>
-		</header>
-		<section id="logobar">
-			<div class="col-sm-6">
-				<figure>
-					<img src="assets/images/logo4.png" alt="RACHANA Library" width="300" height="165">
-				</figure>
-			</div>
-			<div class="col-sm-6 text-right">
+<header>
+	<div class="col-sm-8" id="searchbar">
+		<form method="POST" action="searchbook.php">
+			<h5 style="font-weight: bold; font-size:18px;">Find Any Book of Any Author &nbsp; <span><input type="text"  id="searchbox" name="author" required></span>
+				<button id="searchbtn" type="submit">Search</button></h5>
+		</form>
+	</div>
+	<div class="col-sm-4">
 
-				<button class="btn btn-success" id="btn" onclick="associatedPage('login')">LOGIN</button>
-				<button class="btn btn-success" id="btn" onclick="associatedPage('register')">REGISTER</button>
+<h4 id="welcometext">
+	Welcome &nbsp;<span id="username">
+		<?php 
+	
+	if($_COOKIE['name']){
+	echo $_COOKIE['name'];
+}else{
+	echo 'Guest';
+}
+			 ?></span> &nbsp;
+			<i class="fa fa-user-circle" style="font-size:22px;"></i></h4>
+	</div>
+</header>
+<section id="logobar">
+	<div class="col-sm-6">
+		<figure>
+			<img src="assets/images/logo4.png" alt="RACHANA Library" width="300" height="165">
+		</figure>
+	</div>
 
-			</div>
-		</section>
+	<div class="col-sm-6 text-right" id="preLoginBtns">
+		<button class="btn btn-success" id="btn" onclick="associatedPage('login')">LOGIN</button>
+		<button class="btn btn-success" id="btn" onclick="associatedPage('register')">REGISTER</button>
+	</div>
 
-		<section id="navlist">
-			<div class="col-sm-12" id="navbar">
-				<ul class="nav navbar-nav">
-					<li><a href="#" class="active" onclick="homePage()">HOME</a></li>
-					<li><a href="#" onclick="associatedPage('about')">ABOUT US</a></li>
-					<li><a href="#" onclick="associatedPage('collection')">COLLECTION</a></li>
-					<li><a href="#" onclick="associatedPage('newadd')">NEWLY ADDED</a></li>
-					<li><a href="#" onclick="associatedPage('popular')">MOST POPULAR</a></li>
-					<li><a href="#" onclick="associatedPage('involved')">GET INVOLVED</a></li>
+</section>
 
-				</ul>
+<section id="navlist">
+	<div class="col-sm-12" id="navbar">
+		<ul class="nav navbar-nav">
+			<li><a href="#" class="active" onclick="homePage()">HOME</a></li>
+			<li><a href="#about" onclick="associatedPage('about')">ABOUT US</a></li>
+			<li><a href="#collection" onclick="associatedPage('collection')">COLLECTION</a></li>
+			<li><a href="#newadd" onclick="associatedPage('newadd')">NEWLY ADDED</a></li>
+			<li><a href="#popular" onclick="associatedPage('popular')">MOST POPULAR</a></li>
+			<li><a href="#involved" onclick="associatedPage('involved')">GET INVOLVED</a></li>
 
-			</div>
-		</section>
-		
-			
-		<section>
-			<!--Float-button-->
-			<div class="contactUs-float">
-				<a href="#follow" alt="contact us for your next web job">Contact Us</a>
-			</div>
-		</section>
+		</ul>
+
+	</div>
+</section>
+
+
+<section>
+	<!--Float-button-->
+	<div class="contactUs-float">
+		<a href="#follow" alt="contact us for your next web job">Contact Us</a>
+	</div>
+</section>
